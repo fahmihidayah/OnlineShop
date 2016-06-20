aae38617b29404099572c07d37261430950868ae
Damian Bartos <damian483@gmail.com>
2016-06-20 21:19:52 +0200

    add properties and move to controllers package

--- src/main/java/onlineShop/OnlineShopApp.java
+++ src/main/java/onlineShop/OnlineShopApp.java
@@ -3,11 +3,13 @@ package onlineShop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by Damian Bartos on 20.06.2016.
  */
 @EnableAutoConfiguration
+@ComponentScan({"controllers"})
 @SpringBootApplication
 public class OnlineShopApp {
     public static void main(String[] args) {
