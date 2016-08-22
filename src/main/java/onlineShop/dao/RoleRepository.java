a8900370dfb0964eea9b798ab23ea9cd27a9ab78
Damian Bartos <damian483@gmail.com>
2016-08-22 15:09:31 +0200

    rolerepo changes

--- src/main/java/onlineShop/dao/RoleRepository.java
+++ src/main/java/onlineShop/dao/RoleRepository.java
@@ -2,10 +2,12 @@ package onlineShop.dao;
 
 import onlineShop.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Damian Bartos
  * 22.08.2016.
  */
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
