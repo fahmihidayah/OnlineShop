e236e075abb0ceaa7cc2736253a414031d3a30ab
Damian Bartos <damian483@gmail.com>
2016-08-23 16:23:41 +0200

    smal changes

--- src/main/java/onlineShop/controllers/Controller.java
+++ src/main/java/onlineShop/controllers/Controller.java
@@ -1,15 +1,18 @@
 package onlineShop.controllers;
 
 import onlineShop.dao.AddressRepository;
-import onlineShop.dao.ItemRepository;
 import onlineShop.dao.OrderRepository;
 import onlineShop.dao.RoleRepository;
 import onlineShop.domain.Item;
 import onlineShop.domain.Role;
+import onlineShop.domain.User;
+import onlineShop.services.ItemService;
 import onlineShop.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 /**
  * Created by Damian Bartos
@@ -20,7 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
 
     @Autowired
-    UserServices userServices;
+    UserServices userService;
+
     @Autowired
     AddressRepository addressRepository;
 
@@ -28,21 +32,13 @@ public class Controller {
     RoleRepository roleRepository;
 
     @Autowired
-    ItemRepository itemRepository;
-
+    ItemService itemService;
     @Autowired
     OrderRepository orderRepository;
-    @RequestMapping("/i")
-    public String items(){
-        Item i1 = new Item(1, "item1");
-        Item i2 = new Item(2, "item2");
-        Item i3 = new Item(3, "item3");
-        Item i4 = new Item(4, "item4");
-        itemRepository.save(i1);
-        itemRepository.save(i2);
-        itemRepository.save(i3);
-        itemRepository.save(i4);
-        return "item added";
+
+    @RequestMapping("/item")
+    public Item items(){
+        return itemService.createItem(3213, "item 1", new BigDecimal(12.33));
     }
 
     @RequestMapping("/or")
@@ -52,8 +48,7 @@ public class Controller {
     }
 
     @RequestMapping("/reg")
-    public String reg(){
-        userServices.registerUser("damian483", "32sssd11", "damian483@wp.pl", Role.USER);
-        return "ok\n";
+    public User reg(){
+        return userService.registerUser("damian483", "32sssd11", "damian483@wp.pl", Role.USER());
     }
 }
