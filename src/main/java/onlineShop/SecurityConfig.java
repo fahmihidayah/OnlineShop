e293419499529fdeac9c1b42c570c1aa9b314e38
Damian Bartos <damian483@gmail.com>
2016-08-26 14:00:53 +0200

    fix

--- src/main/java/onlineShop/SecurityConfig.java
+++ src/main/java/onlineShop/SecurityConfig.java
@@ -1,7 +1,9 @@
 package onlineShop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,5 +19,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //TODO: add spring security configuration
+    }
+//    @Autowired
+    javax.sql.DataSource dataSource;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .usersByUsernameQuery("select login, hashed_password from users where login=?")
+                .authoritiesByUsernameQuery("select login, role_name from users, roles where login=?");
     }
 }
