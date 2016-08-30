d02ae5efaddd01cbda3cc76aed87261d9cea8349
Damian Bartos <damian483@gmail.com>
2016-08-30 14:08:15 +0200

    simple task

--- src/main/java/onlineShop/OnlineShopApp.java
+++ src/main/java/onlineShop/OnlineShopApp.java
@@ -5,14 +5,17 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- * Created by Damian Bartos on 20.06.2016.
+ * Created by Damian Bartos
+ * 20.06.2016.
  */
 @EnableAutoConfiguration
 @EnableConfigurationProperties
 @ComponentScan({"onlineShop"})
 @SpringBootApplication
+@EnableScheduling
 public class OnlineShopApp {
     public static void main(String[] args) {
         SpringApplication.run(OnlineShopApp.class, args);
---
+++ src/main/java/onlineShop/Utils/TaskTest.java
@@ -0,0 +1,25 @@
+package onlineShop.Utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Damian Bartos
+ * 30.08.2016.
+ */
+@Component
+public class TaskTest {
+    private static final Logger log = LoggerFactory.getLogger(TaskTest.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+//    @Scheduled(cron = "0 */1 * * * *")
+//    @Scheduled(fixedRate = 5000)
+    public void test(){
+        log.info("TASK: ", dateFormat.format(new Date()));
+    }
+}
