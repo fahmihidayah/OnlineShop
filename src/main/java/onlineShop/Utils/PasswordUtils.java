7984015477af68103b251e2c2eb261cc17fdd153
Damian Bartos <damian483@gmail.com>
2016-08-30 14:26:19 +0200

    add password util for future hashed password method

---
+++ src/main/java/onlineShop/Utils/PasswordUtils.java
@@ -0,0 +1,12 @@
+package onlineShop.Utils;
+
+/**
+ * Created by Damian Bartos
+ * 30.08.2016.
+ */
+public class PasswordUtils {
+    public static String hash(String hashedPassword) {
+        //TODO: change hash method
+        return hashedPassword+"QWERTY";
+    }
+}
