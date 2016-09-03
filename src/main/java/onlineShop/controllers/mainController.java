2dce28bab07a99c5d67131c5a275779bd7649ba6
Damian Bartos <damian483@gmail.com>
2016-09-03 16:48:43 +0200

    MainController

---
+++ src/main/java/onlineShop/controllers/mainController.java
@@ -0,0 +1,16 @@
+package onlineShop.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Damian Bartos
+ * 03.09.2016.
+ */
+@Controller
+public class MainController {
+
+    //TODO: add exclude paths to other than api/*
+    public void exclude(){
+    }
+
+}
