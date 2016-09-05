e6adf193a01e73a352f1b436af755de66889f791
Damian Bartos <damian483@gmail.com>
2016-09-05 21:49:47 +0200

    test registry now works

--- src/main/resources/static/app/components/register/register-controller.js
+++ src/main/resources/static/app/components/register/register-controller.js
@@ -4,18 +4,43 @@
  */
 
 angular.module('onlineShop.register', ['ui.bootstrap', 'ngRoute', 'angularValidator'])
-    .controller('RegisterController', function ($scope) {
-        $scope.firstName = '';
-        $scope.lastName = '';
-        $scope.login = '';
-        $scope.email = '';
-        $scope.password = '';
-        $scope.submitForm = function() {
-            if ($scope.registerForm.$valid) {
-                //login here
+    .controller('RegisterController', function ($scope, $http) {
+        $scope.myForm = {};
+        $scope.user = {};
+        $scope.submitMyForm = function(){
+            console.log($scope.myForm);
+            console.log($scope.user);
+            $http({
+                method: 'POST',
+                url: '/test/test',
+                data: angular.toJson($scope.user)
+            }).success(function(){
+                console.log('wyslano');
+            })
+        };
+        $scope.loginValidator = function(login){
+            if(!login){return}
+            if(login.length<4){
+                return "login must be at least " + 4 + " characters long";
+            }
+            return true;
+        };
+        $scope.passwordValidator = function(password) {
 
+            if(!password){return;}
+
+            if (password.length < 6) {
+                return "Password must be at least " + 6 + " characters long";
             }
 
-        };
+            if (!password.match(/[A-Z]/)) {
+                return "Password must have at least one capital letter";
+            }
 
+            if (!password.match(/[0-9]/)) {
+                return "Password must have at least one number";
+            }
+
+            return true;
+        };
     });
