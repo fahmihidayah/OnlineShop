19ec7709cf4f0adb7eda2acd367ce99b19438732
Damian Bartos <damian483@gmail.com>
2016-09-06 14:50:35 +0200

    add single item view

--- src/main/resources/static/app/components/item/item-controller.js
+++ src/main/resources/static/app/components/item/item-controller.js
@@ -3,8 +3,32 @@
  * 06.09.2016.
  */
 
-angular.module('onlineShop.item', ['ngRoute'])
+angular.module('onlineShop.item', ['ngRoute', 'ui.bootstrap'])
     .controller('ItemController', ['$scope', '$routeParams', '$rootScope', function($scope, $routeParams, $rootScope){
-        console.log($rootScope.test);
-        console.log($routeParams.id);
+        $scope.myInterval = 5000;
+        $scope.noWrapSlides = false;
+        $scope.active = 0;
+        var slides = $scope.slides = [];
+        var currIndex = 0;
+
+        $scope.init = function(){
+            var itemIndex = $routeParams.id;
+            //download item info
+            slides.push({
+                image: '/assets/img/maxresdefault.jpg',
+                id: currIndex++
+            });
+            slides.push({
+                image: '/assets/img/test.png',
+                id: currIndex++
+            });
+            slides.push({
+                image: '/assets/img/maxresdefault.jpg',
+                id: currIndex++
+            });
+            slides.push({
+                image: '/assets/img/test.png',
+                id: currIndex++
+            });
+        }
     }]);
