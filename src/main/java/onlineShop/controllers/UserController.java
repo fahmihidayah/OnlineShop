cbbe151413a48d2874a63ea9f423eab452bc326a
Damian Bartos <damian483@gmail.com>
2016-09-07 18:21:50 +0200

    add route for principals

--- src/main/java/onlineShop/controllers/UserController.java
+++ src/main/java/onlineShop/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 /**
@@ -20,6 +21,11 @@ import java.util.List;
 public class UserController {
     @Autowired
     UserService userService;
+
+    @RequestMapping(value = "/user")
+    public Principal user(Principal user){
+        return user;
+    }
 
     @RequestMapping(value = Route.USER_LIST)
     public List<User> getAllUser(Pageable pageable){
