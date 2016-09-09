e332bddeb3101fe8f5ac80aa6cd7125fb04c9120
Damian Bartos <damian483@gmail.com>
2016-09-09 19:44:01 +0200

    add controllers

---
+++ src/main/java/onlineShop/controllers/ShoppingCartController.java
@@ -0,0 +1,11 @@
+package onlineShop.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Damian Bartos
+ * 09.09.2016.
+ */
+@Controller
+public class ShoppingCartController {
+}
