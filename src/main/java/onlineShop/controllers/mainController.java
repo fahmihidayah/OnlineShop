8ec8a0b67a694b3e96f0713e66cf64d4ae41c030
Damian Bartos <damian483@gmail.com>
2016-09-09 18:42:46 +0200

    add initData

--- src/main/java/onlineShop/controllers/mainController.java
+++ src/main/java/onlineShop/controllers/mainController.java
@@ -1,6 +1,15 @@
 package onlineShop.controllers;
 
+import onlineShop.domain.Item;
+import onlineShop.domain.Role;
+import onlineShop.domain.User;
+import onlineShop.services.IItemService;
+import onlineShop.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.math.BigDecimal;
 
 /**
  * Created by Damian Bartos
@@ -8,6 +17,30 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class MainController {
+    @Autowired
+    IUserService userService;
+
+    @Autowired
+    IItemService itemService;
+
+    @RequestMapping(value = "public/initData")
+    public void initDataIfNeeded(){
+        User user = new User("damian483", "Damian1", "damian483@wp.pl");
+        user.setRole(Role.USER());
+        user.setFirstName("Damian");
+        user.setLastName("Bartos");
+        userService.create(user);
+        User admin = new User("admin", "Admin", "admin@wp.pl");
+        admin.setRole(Role.ADMIN());
+        admin.setFirstName("Admin");
+        admin.setLastName("Nimda");
+        userService.create(admin);
+
+        Item item = new Item(9328, "Testowy Przedmiot", new BigDecimal(123.22));
+        item.setQuantity(12);
+        item.setDescription("JAKIS TAM OPIS");
+        itemService.create(item);
+    }
 
     //TODO: add exclude paths to other than api/*
     public void exclude(){
