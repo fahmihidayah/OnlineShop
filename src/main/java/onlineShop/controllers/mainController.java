7e7bbe808b00668f7dc581be115f85f72610feb6
Damian Bartos <damian483@gmail.com>
2016-09-11 18:13:29 +0200

    modify init data

--- src/main/java/onlineShop/controllers/mainController.java
+++ src/main/java/onlineShop/controllers/mainController.java
@@ -2,9 +2,11 @@ package onlineShop.controllers;
 
 import onlineShop.domain.Item;
 import onlineShop.domain.Role;
+import onlineShop.domain.ShoppingCart;
 import onlineShop.domain.User;
 import onlineShop.services.IItemService;
 import onlineShop.services.IUserService;
+import onlineShop.services.ShoppingCartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,17 +25,24 @@ public class MainController {
     @Autowired
     IItemService itemService;
 
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
     @RequestMapping(value = "public/initData")
     public void initDataIfNeeded(){
         User user = new User("damian483", "Damian1", "damian483@wp.pl");
         user.setRole(Role.USER());
         user.setFirstName("Damian");
         user.setLastName("Bartos");
+        ShoppingCart sc = shoppingCartService.create();
+        user.setShoppingCart(sc);
         userService.create(user);
         User admin = new User("admin", "Admin", "admin@wp.pl");
         admin.setRole(Role.ADMIN());
         admin.setFirstName("Admin");
         admin.setLastName("Nimda");
+        ShoppingCart sc2 = shoppingCartService.create();
+        admin.setShoppingCart(sc2);
         userService.create(admin);
 
         Item item = new Item(9328, "Testowy Przedmiot", new BigDecimal(123.22));
